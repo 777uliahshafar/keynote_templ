5803ec0d315c34eab54bf6976f701c3562b2e237
777uliahshafar <777uliahshafar@gmail.com>
2024-06-15 17:23:01 +0800

    rnmescript2

--- figures/rename.ps1
+++ figures/rename.ps1
@@ -1,6 +1,6 @@
 $path = "."
 $fileTypes = ".*.jpg|.*.bmp|.*.png|.*.gif|.*.tif"
-$files = Get-ChildItem -Recurse | Where-Object FullName -Match ".*$path*"
+$files = Get-ChildItem -exclude "bg.jpg", "chamaeleon_hochformat.png", "guide.png", "kopfhoerer.png" -Recurse | Where-Object FullName -Match ".*$path*"
 $counter = 1
 $dir = ""
 
@@ -8,6 +8,8 @@ foreach ($file in $files) {
     $name = $file.Name
     $fullname = $file.FullName
     $extension = $file.Extension
+$random = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 4 | ForEach-Object {[char]$_})
+$number = Get-Random -Min (11) -Max 99
 
     if ($name -Match $fileTypes) {
         if ($dir -ne $file.Directory.Name) {
@@ -17,7 +19,7 @@ foreach ($file in $files) {
 
         $zero = If ( $counter -le 9) { "0" } Else { "" }
 
-        Rename-Item $fullname "$zero$counter-$dir$extension"
+        Rename-Item $fullname "$number-$random$extension"
 
         $counter++
     }
