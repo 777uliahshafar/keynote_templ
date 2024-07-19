cddf73f2a5ab044ba793f445bded10c16b7a0391
Uliah Shafar <67497706+777uliahshafar@users.noreply.github.com>
2024-07-19 13:46:21 +0800

    rname

--- figures/rename.ps1
+++ figures/rename.ps1
@@ -1,6 +1,7 @@
 $path = "."
 $fileTypes = ".*.jpg|.*.bmp|.*.png|.*.gif|.*.tif"
 $files = Get-ChildItem -exclude "bg.jpg", "chamaeleon_hochformat.png", "guide.png", "kopfhoerer.png" -Recurse | Where-Object FullName -Match ".*$path*"
+//script won't work without the existing of exclusion files.
 $counter = 1
 $dir = ""
 
